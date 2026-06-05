53248aca0cdcbf41444134d9f5b20976d01d89cd
takumi <Takubon1202103@icloud.com>
2026-06-05 15:32:01 +0900

    Revert "fix: 動画の比率を4:3に固定"
    This reverts commit 7310fb1816a876d77d41cbe1075691cc88058202.

--- lib/screens/timeline_screen.dart
+++ lib/screens/timeline_screen.dart
@@ -519,7 +519,7 @@ class _VideoPageState extends State<_VideoPage> {
                 if (controller != null && controller.value.isInitialized)
                   Center(
                     child: AspectRatio(
-                      aspectRatio: 4 / 3,
+                      aspectRatio: controller.value.aspectRatio,
                       child: Builder(builder: (_) {
                         final f = kVideoFilters.firstWhere(
                           (f) => f.name == widget.filterName,
